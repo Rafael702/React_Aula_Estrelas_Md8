c4677f72c69053cb002c67e57f479a7a94c04ba9
Rafael702 <rafaeldealmeida.192001@gmail.com>
2022-01-13 08:09:15 -0300

    Incluir alteracao de nome da props.

--- src/componentes/ComponentesComFilhos.js
+++ src/componentes/ComponentesComFilhos.js
@@ -2,13 +2,9 @@ import react from "react";
 
 function ComponenteComFilhos(props) {
   return (
-    <div>
-      {props.titulo}
       <div>
-          {props.filho}
+          {props.children}
       </div>
-
-    </div>
   );
 }
 
